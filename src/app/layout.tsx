2530b852a443225b2709365c654398287da22c84
mayank-gupta201 <mayankgoyal3005@gmail.com>
2025-06-12 02:15:38 +0530

    work in APIs, middleware & Auth Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      </AuthProvider>
     </html>
   );
 }
